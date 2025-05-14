6460844724793e64ffeb1419a517c82739a62e0d
jenri019 <j.enrique209436@hotmail.com>
2025-05-13 18:33:18 -0600

    feat: adding routerlink to nav

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { Router, RouterLink } from '@angular/router';
+import { tap } from 'rxjs';
 
 @Component({
-  selector: 'app-navbar',
-  imports: [],
-  templateUrl: './navbar.component.html',
+    selector: 'app-navbar',
+    imports: [
+        RouterLink
+    ],
+    templateUrl: './navbar.component.html',
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+    router = inject(Router);
+    routes = routes.map((route) => ({
+        path: route.path,
+        title: `${route.title ?? 'Mapas'}`,
+    }));
+
+    pageTitle$ = this.router.events.pipe(
+        tap(event => {
+            console.log(event);
+        })
+    )
+}
