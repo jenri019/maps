6098320136588ea7ee2141190dc6ab0f3c9e6938
jenri019 <j.enrique209436@hotmail.com>
2025-05-14 11:57:13 -0600

    feat: using mapview component to show map, adding markers array to display markers

--- src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -1,43 +1,13 @@
-import { DecimalPipe } from '@angular/common';
-import { Component, signal, ViewChild } from '@angular/core';
-import { MapComponent, MarkerComponent } from '@maplibre/ngx-maplibre-gl';
+import { Component } from '@angular/core';
+import { MapViewComponent } from "../../components/map-view/map-view.component";
 
 @Component({
     selector: 'app-fullscreen-map-page',
     imports: [
-        MapComponent,
-        //MarkerComponent,
-        DecimalPipe
-    ],
+    //MarkerComponent,
+    MapViewComponent
+],
     templateUrl: './fullscreen-map-page.component.html',
 })
 export class FullscreenMapPageComponent {
-    zoom = signal(1);
-
-    @ViewChild('mapLibre', { static: false }) mapComponent?: any;
-
-    onZoomInput(value: number) {
-        this.zoom.set(+value);
-        if (this.mapComponent && this.mapComponent.mapInstance) {
-            this.mapComponent.mapInstance.setZoom(+value);
-        }
-    }
-
-    centerMap() {
-        const coords = [-65.017, -16.457]; // Cambia por las coordenadas que desees
-        if (this.mapComponent && this.mapComponent.mapInstance) {
-            this.mapComponent.mapInstance.setCenter(coords);
-        }
-    }
-
-    onMapMove(event: any) {
-        const zoom = event.target?.getZoom?.();
-        if (zoom !== undefined) {
-            this.zoom.set(zoom);
-        }
-    }
-
-    alert(message: string) {
-        alert(message);
-    }
 }
--- src/app/pages/markers-page/markers-page.component.ts
+++ src/app/pages/markers-page/markers-page.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MapViewComponent } from "../../components/map-view/map-view.component";
 
 @Component({
-  selector: 'app-markers-page',
-  imports: [],
-  templateUrl: './markers-page.component.html',
+    selector: 'app-markers-page',
+    imports: [
+    MapViewComponent
+],
+    templateUrl: './markers-page.component.html',
 })
-export class MarkersPageComponent { }
+export class MarkersPageComponent {
+    zoom = signal(1);
+    markers = [
+        {
+            id: 1,
+            coordinates: [-65.017, -16.457],
+            title: 'Marker 1',
+        },
+        {
+            id: 2,
+            coordinates: [-165.017, 16.457],
+            title: 'Marker 2',
+        }
+    ];
+}
