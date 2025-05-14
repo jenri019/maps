e86f9c30661772bcbc3811aff65a170a8b7c9126
jenri019 <j.enrique209436@hotmail.com>
2025-05-13 18:46:41 -0600

    feat: showing route title in navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { routes } from '../../../app.routes';
-import { Router, RouterLink } from '@angular/router';
-import { tap } from 'rxjs';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-navbar',
     imports: [
+        AsyncPipe,
         RouterLink
     ],
     templateUrl: './navbar.component.html',
@@ -14,12 +17,31 @@ export class NavbarComponent {
     router = inject(Router);
     routes = routes.map((route) => ({
         path: route.path,
-        title: `${route.title ?? 'Mapas'}`,
-    }));
+        title: `${route.title ?? 'Mapas en NG'}`,
+    })).filter((route) => {
+        return route.path !== '**'
+    });
 
+    // Getting page title as a obervable
     pageTitle$ = this.router.events.pipe(
-        tap(event => {
-            console.log(event);
+        filter((event) => event instanceof NavigationEnd),
+        tap((event) => console.log(event)),
+        map((event) => {
+            return event.url
+        }),
+        map((url) => {
+            return routes.find((route) => `/${route.path}` === url)?.title ?? 'Mapas en NG'
         })
     )
+    // Getting page title as a signal
+    /* pageTitle = toSignal(this.router.events.pipe(
+        filter((event) => event instanceof NavigationEnd),
+        tap((event) => console.log(event)),
+        map((event) => {
+            return event.url
+        }),
+        map((url) => {
+            return routes.find((route) => `/${route.path}` === url)?.title ?? 'Mapas en NG'
+        })
+    )); */
 }
