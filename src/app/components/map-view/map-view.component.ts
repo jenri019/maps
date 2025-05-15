d8e9b80086ee709dee80ddb08dc7ca12392ac3d2
jenri019 <j.enrique209436@hotmail.com>
2025-05-14 21:27:38 -0600

    feat: aadding funciotns to add markers, move to it whit animation & show markers list

--- src/app/components/map-view/map-view.component.ts
+++ src/app/components/map-view/map-view.component.ts
@@ -1,14 +1,23 @@
-import { DecimalPipe } from '@angular/common';
-import { AfterViewInit, Component, input, signal, ViewChild } from '@angular/core';
+import { CommonModule, DecimalPipe, JsonPipe } from '@angular/common';
+import { Component, input, signal, ViewChild } from '@angular/core';
 import { MapComponent, MarkerComponent } from '@maplibre/ngx-maplibre-gl';
 import { LngLatLike, MapLayerMouseEvent } from 'maplibre-gl';
+import { v4 as UUIDV4 } from 'uuid';
+
+interface MapMarker {
+    coordinates: LngLatLike;
+    id: string;
+    color: string;
+}
 
 @Component({
     selector: 'app-map-view',
     imports: [
+        CommonModule,
         MapComponent,
         MarkerComponent,
-        DecimalPipe
+        DecimalPipe,
+        JsonPipe
     ],
     templateUrl: './map-view.component.html',
 })
@@ -17,23 +26,17 @@ export class MapViewComponent {
     zoom = signal(1);
     mapType = input<'fullscreen' | 'markers'>('fullscreen');
     /* markersValue = input<any[]>([]); */
-    markers = signal<any[]>([]);
+    markers = signal<MapMarker[]>([]);
+    selectedMarker = signal<MapMarker>(null);
 
     @ViewChild('mapLibre', { static: false }) mapLibre?: MapComponent;
 
+    /** Funciones generales */
     /** Zoom en el mapa usando el input range */
     onZoomInput(value: number) {
         this.zoom.set(+value);
         if (this.mapLibre && this.mapLibre.mapInstance) {
             this.mapLibre.mapInstance.setZoom(+value);
-        }
-    }
-
-    /** Mover el mapa a 0,0 */
-    centerMap() {
-        const coords = [-65.017, -16.457] as LngLatLike; // Cambia por las coordenadas que desees
-        if (this.mapLibre && this.mapLibre.mapInstance) {
-            this.mapLibre.mapInstance.setCenter(coords);
         }
     }
 
@@ -45,15 +48,27 @@ export class MapViewComponent {
         }
     }
 
-    onMapClick(event: MapLayerMouseEvent) {
-        const coordinates: [number, number] = [event.lngLat.lng, event.lngLat.lat];
-        this.markers.update(current => [...current, { coordinates }]);
+    /**Funciones solo disponibles para la ruta markers */
+    addMarker(event: MapLayerMouseEvent) {
+        if (this.mapType() !== 'markers') return;
+        const marker: MapMarker = {
+            coordinates: [event.lngLat.lng, event.lngLat.lat],
+            id: UUIDV4(),
+            color: this.getRandomHexColor(),
+        }
+        this.markers.update(current => [...current, marker]);
     }
 
     /**Mover el mapa basado en coordenadas */
-    moveMap(coords: number[]) {
+    viewMarker(marker: MapMarker) {
+        if (this.mapType() !== 'markers') return;
         if (this.mapLibre && this.mapLibre.mapInstance) {
-            this.mapLibre.mapInstance.setCenter(coords as LngLatLike);
+            this.mapLibre.mapInstance.easeTo({ center: marker.coordinates, duration: 600 }); // animación suave
+            this.selectedMarker.set(marker);
         }
+    }
+
+    getRandomHexColor(): string {
+        return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
     }
 }
